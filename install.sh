10719677eb946b5858ef0e032bad77ed0d338aca
Yatish Mehta <yatishmehta27@gmail.com>
2014-02-26 22:10:57 +0530

    Added header for install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
+#!/usr/bin/env bash
 echo "Copying files from repo to $HOME"
 cp ./gitconfig ~/.gitconfig
 cp ./irbrc ~/.irbrc
-cp ./.zshrc ~/.zshrc
-cp ./.pryrc ~/.pryrc
+cp ./zshrc ~/.zshrc
+cp ./pryrc ~/.pryrc
 cp ./gemrc ~/.gemrc
 echo "Copied gitconfig irbrc gemrc pryrc"
 
