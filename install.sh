f519be199facc70643100469fd025c5c18e1a918
Yatish Mehta <yatishmehta27@gmail.com>
2015-01-08 13:50:59 -0500

    Added plugin setup

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+git clone https://github.com/yatish27/dotfiles.git ~/.dotfiles
+cd ~/.dotfiles
+
 echo "Copying files from repo to $HOME"
 cp ./gitconfig ~/.gitconfig
 cp ./irbrc ~/.irbrc
@@ -9,3 +12,11 @@ cp ./rspec ~/.rspec
 cp ./vimrc ~/.vimrc
 cp ./vimrc.bundles ~/.vimrc.bundles
 echo "Copied gitconfig zshrc irbrc gemrc pryrc aliases vimrc"
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +BundleInstall +qall
+echo "Yatish's vim setup done"
+
+cd ~
+rm -rf ~/.dotfiles
+
--- vimsetup.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +BundleInstall +qall
-echo "Yatish's vim setup done"
